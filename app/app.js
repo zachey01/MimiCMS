d87fb98d831cd818699e95a70b2efde808d2357c
Zachey <fradexo56@gmail.com>
2023-06-05 20:59:04 +0500

    upd

--- app/app.js
+++ app/app.js
@@ -148,11 +148,37 @@ app.get(
   }
 );
 
-app.get("/profile", (req, res) => {
+app.get("/profile", function (req, res) {
+  let avatar = "";
   if (userSteamID) {
-    res.send(`Steam ID пользователя: ${userSteamID}`);
+    pool.query(
+      `SELECT avatar, balance FROM users WHERE steamid = '${userSteamID}'`,
+      (error, results, fields) => {
+        if (error) throw error;
+        avatar = results[0].avatar;
+        balance = results[0].balance;
+        const authVars = {
+          logo: process.env.LOGO,
+          currency: process.env.CURRENCY,
+          slide_1: process.env.SLIDE_1,
+          slide_2: process.env.SLIDE_2,
+          slide_3: process.env.SLIDE_3,
+          tg_channel: process.env.TG_CHANNEL,
+          discord_server_id: process.env.DISCORD_SERVER_ID,
+          name: process.env.NAME,
+          avatar: avatar,
+          balance: balance,
+          userName: userName,
+          steamLink: `https://steamcommunity.com/id/${userSteamID}`,
+        };
+        res.render(
+          path.join(__dirname, "views", "./user-profile.ejs"),
+          authVars
+        );
+      }
+    );
   } else {
-    res.send("Вы не авторизован");
+    res.send("Вы не авторизованы");
   }
 });
 
